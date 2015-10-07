f02d4adade9717a705fd29675e67bbcf6372cc95
Jane <janecakemaster@gmail.com>
2015-10-06 22:15:34 -0400

    some regex for fun

--- app/regex.js
+++ app/regex.js
@@ -7,12 +7,12 @@ exports.regexAnswers = {
   },
 
   containsRepeatingLetter : function(str) {
-    var re = /[0-9]/;
+    var re = /([a-zA-Z]).*?\1/;
     return re.test(str);
   },
 
   endsWithVowel : function(str) {
-    var re = /[0-9]/;
+    var re = /[aeiou]$/;
     return re.test(str);
   },
 
@@ -25,7 +25,7 @@ exports.regexAnswers = {
     var re = /[0-9]/;
     return re.test(str);
   },
-  
+
   isUSD : function(str) {
     var re = /[0-9]/;
     return re.test(str);
