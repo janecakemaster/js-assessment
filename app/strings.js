1538ee6c5552589e8ab7b579e0ade474dab5ac52
Jane <janecakemaster@gmail.com>
2015-10-06 22:10:18 -0400

    oops whiel loop

--- app/strings.js
+++ app/strings.js
@@ -13,17 +13,17 @@ exports.stringsAnswers = {
     result = '',
     n = '\n';
 
-    while (words) {
-      var word = words.shift();
-      if(len > cols) {
-        len += word.length;
-        result += word;
-        if(result.length > cols) {
-
-        }
-      }
+    // while (words) {
+    //   var word = words.shift();
+    //   if(len > cols) {
+    //     len += word.length;
+    //     result += word;
+    //     if(result.length > cols) {
+          
+    //     }
+    //   }
       
-    }
+    // }
   },
   reverseString: function(str) {
     return str.split('').reverse().join('');
