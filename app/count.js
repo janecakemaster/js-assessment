3563e17c24bc5ad1af2fa40cad65cc83ca0669d2
Jane <janecakemaster@gmail.com>
2015-10-06 21:05:27 -0400

    add note for count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,8 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-
+    // @note 
+    // TypeError: Cannot read property 'useFakeTimers' of undefined
+    // at Context.<anonymous> (tests/app/count.js:31:23)
   }
 };
