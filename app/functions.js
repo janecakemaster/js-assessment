0cb7d5b9a86ab312f19b19fef1420d5ebb46648f
Jane <janecakemaster@gmail.com>
2015-10-06 21:26:47 -0400

    some functions

--- app/functions.js
+++ app/functions.js
@@ -2,15 +2,15 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(null, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.apply(obj, arguments);
   },
 
   functionFunction : function(str) {
-
+    
   },
 
   makeClosures : function(arr, fn) {
@@ -22,11 +22,16 @@ exports.functionsAnswers = {
   },
 
   useArguments : function() {
-
+    var args = Array.prototype.slice.call(arguments);
+    return args.reduce(function(p, n) {
+      return p + n;
+    });
   },
 
   callIt : function(fn) {
-
+    var args = Array.prototype.slice.call(arguments);
+    args.shift();
+    fn.apply(null, args);
   },
 
   partialUsingArguments : function(fn) {
